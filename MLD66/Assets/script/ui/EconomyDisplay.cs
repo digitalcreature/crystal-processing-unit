15914092df5ad84eae0486a932af748205be33d7
Tanner Grehawick <ggrehawi@gmu.edu>
2016-03-29 21:05:26 -0400

    added bars to eco display, ui tweaks

--- MLD66/Assets/script/ui/EconomyDisplay.cs
+++ MLD66/Assets/script/ui/EconomyDisplay.cs
@@ -7,6 +7,8 @@ public class EconomyDisplay : MonoBehaviour {
 	public Text mineralUsageDisplay;
 	public Text energyCountDisplay;
 	public Text energyUsageDisplay;
+	public Image mineralBar;
+	public Image energyBar;
 
 	void Awake() {
 		if (mineralCountDisplay == null) {
@@ -20,6 +22,8 @@ public class EconomyDisplay : MonoBehaviour {
 		mineralUsageDisplay.text = string.Format("{0:+#.00;-#.00;0.00}", -eco.mineralUsage);
 		energyCountDisplay.text = string.Format("{0:0.00}/{1:0.00} E", eco.energyCount, eco.energyCapacity);
 		energyUsageDisplay.text = string.Format("{0:+#.00;-#.00;0.00}", -eco.energyUsage);
+		mineralBar.fillAmount = eco.mineralCount / eco.mineralCapacity;
+		energyBar.fillAmount = eco.energyCount / eco.energyCapacity;
 	}
 
 }
